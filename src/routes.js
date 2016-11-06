2d2073eaa8d4e2242e90569381e1c9389eae9a1a
oded-soffrin <oded.soffrin@gmail.com>
2016-11-06 22:59:09 +0200

    reduce from error

--- src/routes.js
+++ src/routes.js
@@ -8,13 +8,13 @@ import IgPage from './containers/InstagramPage'; // eslint-disable-line import/n
 import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
 import AboutPage from './components/AboutPage.js';
 import NotFoundPage from './components/NotFoundPage.js';
-import EtsyHomePage from "./containers/EtsyPage"; // eslint-disable-line import/no-named-as-default
+import EtsyPage from "./containers/EtsyPage";
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="gkm" component={GKMHomePage}/>
-        <Route path="etsy" component={EtsyHomePage}/>
+        <Route path="etsy" component={EtsyPage}/>
         <Route path="ig" component={IgPage}/>
         <Route path="fuel-savings" component={FuelSavingsPage}/>
         <Route path="about" component={AboutPage}/>
