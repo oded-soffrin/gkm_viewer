975f890d2226209f436469aaec53006ac94a26f3
oded-soffrin <oded.soffrin@gmail.com>
2016-11-29 15:59:47 +0200

    renmae logo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <img src={require('./src/images/myImage.jpg')} />
+      <img src={require('./src/images/logo.jpg')} />
       <h2>HomePage</h2>
 
     </div>
